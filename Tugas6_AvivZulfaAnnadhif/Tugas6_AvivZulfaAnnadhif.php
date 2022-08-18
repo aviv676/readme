ee520d035b1fb8dacea6e5fa779412a012087d3e
aviv676 <77877792+aviv676@users.noreply.github.com>
2022-08-18 14:51:42 +0700

    Add files via upload

---
+++ Tugas6_AvivZulfaAnnadhif/Tugas6_AvivZulfaAnnadhif.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="content">
+        <h1 id="header">NILAI PESERTA PELATIHAN</h1>
+            <div class="container"><br>
+                    <h3>INPUT NILAI</h3><br>
+                    <h3>Nilai Praktik</h3>
+                    <input class="input" type="text"><br><br>
+                    <h3>Nilai Praktik</h3>
+                    <input class="input" type="text"><br><br>
+                    <h3>Nilai Praktik</h3>
+                    <input class="input" type="text"><br><br>
+                    <h3>Nilai Praktik</h3>
+                    <input class="input" type="text"><br><br><br>
+                    <input class="hitung" type="submit" value="HITUNG"><br><br>
+                    <h3>Nilai Akhir :</h3>
+                    <h3>Kriteria Kelulusan :</h3>
+            </div>
+    </div>
+</body>
+</html>
